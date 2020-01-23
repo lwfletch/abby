131f8802cd2df177245b85da7a35294bbdb88ac9
trey <trey@ip-192-168-0-101.ec2.internal>
2020-01-23 18:29:08 -0500

    Updates

---
+++ precache-manifest.6f0247690e5aaf83050285dceb226f95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6955e9f8a0ba6be32cffc60425111a",
+    "url": "/abby/index.html"
+  },
+  {
+    "revision": "5b6160b0d149194a6a7f",
+    "url": "/abby/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "28aee3cdb2f89a92d5f7",
+    "url": "/abby/static/js/2.3b122401.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/abby/static/js/2.3b122401.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b6160b0d149194a6a7f",
+    "url": "/abby/static/js/main.6fca0688.chunk.js"
+  },
+  {
+    "revision": "b79eeeb9e2b139079f9f",
+    "url": "/abby/static/js/runtime-main.2ba6a7a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/abby/static/media/logo.5d5d9eef.svg"
+  }
+]);
