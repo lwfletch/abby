4db948e27a982f00635e5f76760203cb65256b94
trey <trey@ip-192-168-0-101.ec2.internal>
2020-01-23 18:27:55 -0500

    first commit, pushing out to github pages

--- src/App.js
+++ src/App.js
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello, my is Abby</p>
+        <h4>About Me</h4>
+        <p>i like dogs. and my favorite color is blue</p>
+        <p>Hope you say 2 things about you.</p>
       </header>
     </div>
   );
